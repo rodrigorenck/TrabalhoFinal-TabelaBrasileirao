5c7a56fda3852e210b3905a7d2f95c7bc3ef3d9d
Rodrigo Renck <renckrodrigo030@gmail.com>
2022-06-10 17:20:49 -0300

    First commit

---
+++ Main.java
@@ -0,0 +1,48 @@
+package e6;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Scanner in2 = new Scanner(System.in);
+
+        System.out.print("Informe o codigo do time: ");
+        int codigo = in.nextInt();
+        System.out.print("Informe o time: ");
+        String nome = in2.nextLine();
+
+        Pontuacao pont = new Pontuacao(codigo, nome);
+
+
+        int golsPro;
+        int golsContra;
+        do {
+            System.out.println("Informe quantos gols a favor e quantos gols contra: ");
+            golsPro = in.nextInt();
+            golsContra = in.nextInt();
+        } while (golsPro < 0 || golsContra < 0);
+
+
+        pont.partida(golsPro, golsContra);
+
+        System.out.println(pont);
+
+        System.out.println();
+        System.out.println("OUTRA PARTIDA");
+        do {
+            System.out.println("Informe quantos gols a favor e quantos gols contra: ");
+            golsPro = in.nextInt();
+            golsContra = in.nextInt();
+        } while (golsPro < 0 || golsContra < 0);
+
+        pont.partida(golsPro, golsContra);
+
+        System.out.println(pont);
+
+
+
+
+    }
+}
---
+++ Pontuacao.java
@@ -0,0 +1,54 @@
+package e6;
+
+public class Pontuacao {
+    /*
+    DUVIDAS
+    1. Faco getter para todos?
+    2. Nao vou precisar de setter ne
+    3. Metodo partida esta certo?
+    4. Posso fazer a validacao das entradas no Main?
+     */
+    private int codigo;
+    private String nome;
+    private int pontos;
+    private double jogos;
+    private int vitorias;
+    private int empates;
+    private int derrotas;
+    private int golsPro;
+    private int golsContra;
+    private int saldoGols;
+    private double aproveitamento;
+
+    public Pontuacao(int codigo, String nome){
+        this.codigo = codigo;
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "[CODIGO]" + codigo + " - [TIME]" + nome + " - [PONTOS]" + pontos + " - [JOGOS]" + jogos +
+                " - [VITORIAS]" + vitorias + " - [EMPATES]" + empates + " - [DERROTAS]" + derrotas +
+                " - [GOLS PRO]" + golsPro + " - [GOLS CONTRA]" + golsContra + " - [SALDO GOLS]" + saldoGols +
+                " - [APROVEITAMENTO]" + aproveitamento;
+    }
+
+    public void partida(int golsPro, int golsContra){
+        this.jogos++;
+        if(golsPro>golsContra){
+            this.vitorias++;
+            this.pontos += 3;
+        }else if(golsPro == golsContra){
+            this.empates++;
+            this.pontos += 1;
+        }else{
+            this.derrotas++;
+        }
+        int saldo = golsPro - golsContra;
+        this.saldoGols += saldo;
+        this.golsPro += golsPro;
+        this.golsContra += golsContra;
+        this.aproveitamento = vitorias/jogos;
+    }
+
+}
